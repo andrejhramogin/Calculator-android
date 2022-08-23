598871fa3695af77b09f4396706ec79bd662470b
Yauheni Slizh <tyteishi@gmail.com>
2022-08-23 12:58:56 +0300

    Initial commit.

---
+++ app/src/main/java/com/github/hramogin/calculator/MainActivity.java
@@ -0,0 +1,65 @@
+package com.github.hramogin.calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button openButton;
+    private TextView title;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        openButton = findViewById(R.id.btn_open_main);
+        title = findViewById(R.id.tv_title);
+        openButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        title.setText(R.string.new_title);
+        startActivity(intent);
+    }
+
+//    class MyClickListener implements View.OnClickListener{
+//
+//        @Override
+//        public void onClick(View view) {
+//        `title.setText(R.string.new_title);`
+//        }
+//    }
+}
