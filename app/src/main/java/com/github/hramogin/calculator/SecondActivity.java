f13b36bfcdc3d30b8adafbc25bd6140e0d9fd4f4
andrejhramogin <andrej.hramogin2@gmail.com>
2022-08-24 12:31:41 +0300

    ThirdActivity continuation 24/08/22

--- app/src/main/java/com/github/hramogin/calculator/SecondActivity.java
+++ app/src/main/java/com/github/hramogin/calculator/SecondActivity.java
@@ -1,15 +1,24 @@
 package com.github.hramogin.calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
+
+    @Override
+    public void onClick(View view) {
+    Intent intent = new Intent(this, ThirdActivity.class);
+    startActivity(intent);
+    }
 }
+
