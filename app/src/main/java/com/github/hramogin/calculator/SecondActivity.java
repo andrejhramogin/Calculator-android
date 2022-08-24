b97ef432c7766594289b7dd5ae61a94de51f1316
andrejhramogin <andrej.hramogin2@gmail.com>
2022-08-24 16:01:03 +0300

    ThirdActivity continuation + buttons

--- app/src/main/java/com/github/hramogin/calculator/SecondActivity.java
+++ app/src/main/java/com/github/hramogin/calculator/SecondActivity.java
@@ -3,16 +3,24 @@ package com.github.hramogin.calculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+//import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button openButton;
+    private TextView title;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        openButton = findViewById(R.id.btn_start_calculator);
+        openButton.setOnClickListener(this);
     }
 
     @Override
--- app/src/main/java/com/github/hramogin/calculator/ThirdActivity.java
+++ app/src/main/java/com/github/hramogin/calculator/ThirdActivity.java
@@ -1,7 +1,19 @@
 package com.github.hramogin.calculator;
 
+import android.os.Bundle;
+import android.widget.Button;
+
+//import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ThirdActivity extends AppCompatActivity {
 
+    private Button openButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+    }
 }
